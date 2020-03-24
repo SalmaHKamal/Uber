bc316c69c5044bba0464457a90c77374d3b287a2
Salma Hassan <salmahassan087@gmail.com>
2020-03-24 15:59:45 +0200

    Release Notes :
    1] add first view controller

---
+++ UberApp/Controller/LoginViewController.swift
@@ -0,0 +1,19 @@
+//
+//  LoginViewController.swift
+//  UberApp
+//
+//  Created by Salma Hassan on 3/24/20.
+//  Copyright © 2020 salma. All rights reserved.
+//
+
+import UIKit
+
+class LoginViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        view.backgroundColor = .blue
+    }
+
+}

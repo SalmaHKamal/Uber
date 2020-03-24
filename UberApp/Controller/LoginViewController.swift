b062cb3a120073bf7c4e5f66e2eb14ffec93780a
Salma Hassan <salmahassan087@gmail.com>
2020-03-24 16:27:10 +0200

    Release Notes :
    1] adding components for first view controller

--- UberApp/Controller/LoginViewController.swift
+++ UberApp/Controller/LoginViewController.swift
@@ -9,11 +9,40 @@
 import UIKit
 
 class LoginViewController: UIViewController {
+    
+    // MARK: - Properties
+    private let titleLabel : UILabel = {
+        let label = UILabel()
+        label.text = "uber"
+        label.text = label.text?.uppercased()
+        label.textColor = UIColor(white: 1, alpha: 0.8)
+        label.font = UIFont(name: "Avenir-Light", size: 36)
+        return label
+    }()
+     
+    // MARK: - LifeCycle
 
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        view.backgroundColor = .blue
+        view.backgroundColor = UIColor.init(red: 25/255, green: 25/255 , blue: 25/255, alpha: 1)
+        setupTitleLabel()
     }
+    
+    
+    // MARK: - Helper Methods
 
+    func setupTitleLabel(){
+        view.addSubview(titleLabel)
+        
+        titleLabel.translatesAutoresizingMaskIntoConstraints = false
+        titleLabel.centerXAnchor.constraint(equalTo: view.centerXAnchor).isActive = true
+        titleLabel.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 0).isActive = true
+    }
+    
+    override var preferredStatusBarStyle: UIStatusBarStyle {
+        return .lightContent
+    }
 }
+
+
